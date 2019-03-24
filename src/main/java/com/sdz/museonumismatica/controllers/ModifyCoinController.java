a2062a212061f083814ef293db5dad86e679102b
j.chavero <cmjorge4@gmail.com>
2019-03-24 21:07:04 +0100

    Ampliadas las funcionalidades de coin, sin acabar

---
+++ src/main/java/com/sdz/museonumismatica/controllers/ModifyCoinController.java
@@ -0,0 +1,23 @@
+package com.sdz.museonumismatica.controllers;
+
+import com.sdz.museonumismatica.Coin;
+import com.sdz.museonumismatica.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ModifyCoinController {
+    @Autowired
+    private CoinRepository coinRepo;
+
+    @RequestMapping("/modifyCoin")
+    public String modifySupplier(Coin coin, Model model) {
+
+        coinRepo.delete(coin);
+        coinRepo.save(coin);
+
+        return "coinModified";
+    }
+}
