b0ff24663ee04d009ad85f5c9cfc7a1783a2b5d5
mikamp116 <mikamardanyan@gmail.com>
2019-04-02 20:50:28 +0200

    Merge branch 'master' of /home/mikamp/IdeaProjectsUltimate/PO1SD with conflicts.

--- src/main/java/com/sdz/museonumismatica/controllers/Index.java
+++ src/main/java/com/sdz/museonumismatica/controllers/Index.java
@@ -40,9 +40,22 @@ public class Index {
         CoinModel dos_euros = new CoinModel(2, "Euro", 26, (float) 10.3, metales, "Una moneda muy bonita");
         CoinModel un_euro = new CoinModel(1, "Euro", 16, (float) 7.4, "Cobre", "Random description");
         Supplier mika = new Supplier("123456789A", "Mikayel Mardanyan", 28933, "mikamardanyan@gmail.com", 69696969);
+        Supplier supplier1 = new Supplier("111111111A", "Proveedor Uno", 22222, "mikamardanyan@gmail.com", 690111111);
+
+        Supplier supplier2 = new Supplier("222222222B", "Proveedor Dos", 22222, "proveedor_dos@gmail.com", 690222222);
+        Supplier supplier3 = new Supplier("333333333C", "Proveedor Tres", 33333, "proveedor_tres@gmail.com", 690333333);
+        Supplier supplier4 = new Supplier("444444444D", "Proveedor Cuatro", 4444, "proveedor_cuatro@gmail.com", 669444444);
+        Supplier calderilla = new Supplier("B0000000A", "Calderilla", 28000, "calderilla@sd.es", 690000000);
+
         coinModelRepository.save(dos_euros);
         coinModelRepository.save(un_euro);
         supplierRepository.save(mika);
+        supplierRepository.save(supplier1);
+        supplierRepository.save(supplier2);
+        supplierRepository.save(supplier3);
+        supplierRepository.save(supplier4);
+        supplierRepository.save(calderilla);
+        coinRepository.save(new Coin(dos_euros, 2019, "Madrid", new Date(2018, 10, 21), "Bien preservado", mika));
         coinRepository.save(new Coin(dos_euros, 1987, "Madrid", d1, "Bien preservado", mika));
         coinRepository.save(new Coin(dos_euros, 1987, "Alcobendas", d2, "Mal preservado", mika));
         coinRepository.save(new Coin(un_euro, 2008, "Madrid", d3, "Bien preservado", mika));
